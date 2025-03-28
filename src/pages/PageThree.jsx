6ee37f8ff9c791ed0ece62fb8a70e1a315a85862
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-03-28 11:12:01 +0100

    Adding colors to three.

--- src/pages/PageThree.jsx
+++ src/pages/PageThree.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const PageThree = () => {
+  const [data, setData] = useState([])
+
+  const getData = async () => {
+    const result = await fetch('/options.json')
+    if (!result.ok) {
+        console.log("Can't get the json file.")
+    }
+    const data = await result.json();
+
+    setData(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
-    <div>PageThree</div>
+    <>
+      <main className='main-three'>
+        <header className='header-three'></header>
+        <aside className='aside-three'></aside>
+        <section className='section-three'>
+          <div></div>
+          <div></div>
+        </section>
+        <footer className='footer-three'></footer>
+      </main>
+      
+    </>
   )
 }
 
